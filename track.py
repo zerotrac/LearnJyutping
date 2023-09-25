d5a5ac868ae9a25b5866551f934b149d5dd83c36
zerotrac <734688610@qq.com>
2023-09-24 19:02:29 -0700

    Split JyutCharacter class into JyutCharacterKey and JyutCharacterValue

--- track.py
+++ track.py
@@ -5,40 +5,68 @@ from constants import Constants
 from tools import GetCharacters
 
 
-class JyutCharacter:
-    COLUMNS = 6
-
-    def __init__(self, character: str, jyutping: str, learned: int = 0, tested: int = 0, correct: int = 0, incorrect: int = 0) -> None:
+class JyutCharacterKey:
+    def __init__(self, character: str) -> None:
         self.character = character
+
+    def __eq__(self, other: "JyutCharacterKey") -> bool:
+        return self.character == other.character
+
+    def __hash__(self) -> int:
+        return hash(self.character)
+
+    def ToString(self) -> str:
+        return self.character
+
+class JyutCharacterValue:
+    COLUMNS = 5
+
+    def __init__(self, jyutping: str, learned: int = 0, tested: int = 0, correct: int = 0, incorrect: int = 0) -> None:
         self.jyutping = jyutping
         self.learned = learned
         self.tested = tested
         self.correct = correct
         self.incorrect = incorrect
-    
+
     def ToString(self) -> str:
-        return f"{self.character}\t{self.jyutping}\t{self.learned}\t{self.tested}\t{self.correct}\t{self.incorrect}"
-    
+        return f"{self.jyutping}\t{self.learned}\t{self.tested}\t{self.correct}\t{self.incorrect}"
+
     @staticmethod
-    def FromString(serialized: str) -> "JyutCharacter":
+    def FromString(serialized: str) -> "JyutCharacterValue":
         content = serialized.split("\t")
-        assert len(content) == JyutCharacter.COLUMNS
-        return JyutCharacter(content[0], content[1], int(content[2]), int(content[3]), int(content[4]), int(content[5]))
-    
-    def Duplicate(self) -> "JyutCharacter":
-        return JyutCharacter(self.character, self.jyutping, self.learned, self.tested, self.correct, self.incorrect)
+        assert len(content) == JyutCharacterValue.COLUMNS
+        return JyutCharacterValue(content[0], int(content[1]), int(content[2]), int(content[3]), int(content[4]))
+
+class JyutCharacterHelper:
+    @staticmethod
+    def ToString(key: JyutCharacterKey, value: JyutCharacterValue) -> str:
+        return f"{key.ToString()}\t{value.ToString()}"
+
+    @staticmethod
+    def FromString(serialized: str) -> (JyutCharacterKey, JyutCharacterValue):
+        key, value = serialized.split("\t", maxsplit=1)
+        return JyutCharacterKey(key), JyutCharacterValue.FromString(value)
 
 class Tracker:
     TITLE = ["Character", "Jyutping", "Learned", "Tested", "Correct", "Incorrect"]
 
     def __init__(self) -> None:
-        self.characters = list()
-    
-    def Add(self, character: str, jyutping: str) -> None:
-        self.characters.append(JyutCharacter(character, jyutping))
-    
-    def AddFrom(self, character: JyutCharacter) -> None:
-        self.characters.append(character.Duplicate())
+        self.jyutping_map = dict()
+
+    def AddRaw(self, character: str, jyutping: str) -> None:
+        assert type(character) is str
+        assert type(jyutping) is str
+
+        key = JyutCharacterKey(character)
+        assert key not in self.jyutping_map
+        self.jyutping_map[key] = JyutCharacterValue(jyutping)
+
+    def Add(self, key: JyutCharacterKey, value: JyutCharacterValue) -> None:
+        assert type(key) is JyutCharacterKey
+        assert type(value) is JyutCharacterValue
+
+        assert key not in self.jyutping_map
+        self.jyutping_map[key] = value
 
     @staticmethod
     def ReadFromFile(target_path: str) -> "Tracker":
@@ -46,16 +74,16 @@ class Tracker:
         with open(target_path, "r") as fin:
             title = fin.readline().strip()
             assert title == "\t".join(Tracker.TITLE)
-            assert len(Tracker.TITLE) == JyutCharacter.COLUMNS
+            assert len(Tracker.TITLE) == JyutCharacterValue.COLUMNS + 1
             for seralized in fin:
-                tracker.AddFrom(JyutCharacter.FromString(seralized.strip()))
+                tracker.Add(*JyutCharacterHelper.FromString(seralized.strip()))
         return tracker
 
     def WriteToFile(self, target_path: str) -> None:
         with open(target_path, "w") as fout:
             fout.write("\t".join(Tracker.TITLE) + "\n")
-            for character in self.characters:
-                fout.write(character.ToString() + "\n")
+            for key, value in self.jyutping_map.items():
+                fout.write(JyutCharacterHelper.ToString(key, value) + "\n")
 
     def SampleForLearning(self):
         pass
@@ -67,15 +95,15 @@ def NewProgressTracking(target_path: str = Constants.PROGRESS_TRACKING_PATH) -> 
     if os.path.exists(target_path):
         print(f"File {target_path} already exists.")
         return False
-    
+
     characters = GetCharacters()
     tracker = Tracker()
     for character in characters:
         jyutping = ToJyutping.get_jyutping_list(character)
         assert len(jyutping) == 1
         assert jyutping[0][1] is not None
-        tracker.Add(character, jyutping[0][1])
-    
+        tracker.AddRaw(character, jyutping[0][1])
+
     tracker.WriteToFile(target_path)
     print(f"A new progress tracking file {target_path} is created.")
     return True
